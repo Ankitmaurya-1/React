03ec3b26693eb16444f5face6dd2a289d82a9ee6
Ankit <ankitmaurya9560@gmail.com>
2024-02-29 11:16:24 +0530

    Updated counter

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -7,19 +7,11 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    if (counter < 20) {
-      setCounter(counter + 1);
-    } else {
-      setCounter(0);
-    }
+    setCounter(counter + 1);
   };
 
   const removeValue = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    } else {
-      setCounter(20);
-    }
+    setCounter(counter - 1);
   };
 
   return (
