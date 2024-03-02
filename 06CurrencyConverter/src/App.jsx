204806c5b2a22d58ae0a1741b800fd47b5a99cfe
Ankit <ankitmaurya9560@gmail.com>
2024-03-02 09:53:40 +0530

    Initial Files

---
+++ 06CurrencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-orange-700 bg-slate-900 text-3xl">
+        CurrencyConveter
+      </h1>
+    </>
+  );
+}
+
+export default App;
