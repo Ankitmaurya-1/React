0ac95676149d2d33ebc15e9d6fe16bab97ba21da
Ankit <ankitmaurya9560@gmail.com>
2024-03-05 20:51:50 +0530

    React Router

--- 07ReactRouter/src/App.jsx
+++ 07ReactRouter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="bg-green-600">hello world</h1>
+      <h1>React Router</h1>
     </>
   );
 }
---
+++ 07ReactRouter/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
+
+// react router is all about setting the routes of differnet pages into your webapp
