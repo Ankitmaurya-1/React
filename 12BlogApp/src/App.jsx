5c07443c88bc3a4055314ff19e7577d66c3c2a5b
Ankit <ankitmaurya9560@gmail.com>
2024-04-19 17:35:32 +0530

    Initial Files

--- 12BlogApp/src/App.jsx
+++ 12BlogApp/src/App.jsx
@@ -1,40 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
 import "./App.css";
-import authService from "./Appwrite/auth";
-import { login, logout } from "./store/authSlice";
-import { Footer, Header } from "./Components";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login(userData));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .finally(() => setLoading(false));
-  }, []);
-
-  return !loading ? (
-    <div
-      className="min-h-sc
-  flex flex-wrap content-between bg-gray-500
-  "
-    >
-      <div className="w-full block">
-        <Header />
-        <main>// outlet</main>
-        <Footer />
-      </div>
-    </div>
-  ) : null;
+  return (
+    <>
+      <h1>Ankit Blog</h1>
+    </>
+  );
 }
 
 export default App;
